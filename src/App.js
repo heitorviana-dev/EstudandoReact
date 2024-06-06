6258b9a0fd1f27bcff15679c9a30bbc5f87a6a5e
heitorviana-dev <heitorsoaresviana@gmail.com>
2024-06-06 10:25:58 -0300

    Adicionando lifecycle methods

--- src/App.js
+++ src/App.js
@@ -6,26 +6,70 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      name: 'Heitor',
-      lastname: 'Viana',
-      age: 19,
-      count: 0,
+      posts: [
+        {
+          id: 1,
+          title: 'Titulo 1',
+          body: 'Corpo 1'
+        },
+        {
+          id: 2,
+          title: 'Titulo 2',
+          body: 'Corpo 2'
+        },
+        {
+          id: 3,
+          title: 'Titulo 3',
+          body: 'Corpo 3'
+        }
+      ],
+      counter: 0
     };
+    this.timeoutUpdate = null;
   }
 
-  render(){
-    const {name, lastname, age, count} = this.state;
+  componentDidMount(){ // É um método usado principalmente na realização de operações assíncronas, como requisição de dados de uma API.
+    this.handleTimeout();
+  };
 
-    return(
+  componentDidUpdate(prevProps, prevState){
+    this.handleTimeout();
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+      posts[0].title = "Novo Título";
+
+      this.timeoutUpdate = setTimeout(() => {
+        this.setState({ posts, counter: counter + 1 });
+      }, 1000);
+  };
+
+  render(){
+    const { posts, counter } = this.state;
+
+    const listItems = posts.map((value) => {
+      return(
+        <ul key={value.id}>
+          <li>{value.title}</li>
+          <li>{value.body}</li>
+        </ul>
+      );
+    });
+
+    return( // O que está dentro do return é jsx, caso você queira usar alguma lógica do js, é necessário usar as {}
       <div className="App">
           <header className="App-header">
+            <h1>{ counter }</h1>
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Meu nome é {name} {lastname}.
-              <br></br>
-              Tenho {age} anos.
-            </p>
-            <button onClick={this.handlePClick}>{count}</button>
+            
+            <ul>{listItems}</ul>
+
+            <button onClick={this.handlePClick}>{}</button>
             <a
               className="App-link"
               href="https://reactjs.org"
